dad4ef04257887ddbeafeb52018f46aae9f99d04
CaioVRS21 <veras.caio12@gmail.com>
2022-05-25 08:11:11 -0400

    Atualização
    Versão web completa

--- main.js
+++ main.js
@@ -9,48 +9,45 @@ $(document).ready(function() {
 });
 
 function mostrarForm(){
-    let x = document.getElementById('novoComent')
-    let y = document.getElementById('conteudo-disc')
-    let z = document.getElementById('caixa_novo_coment')
-    x.style.display = "block";
-    y.style.display = "none";
-    // z.style.display = "block";
+    const novoComent = document.getElementById('novoComent')
+    const conteudoDisc = document.getElementById('conteudo-disc')
+    novoComent.style.display = "block";
+    conteudoDisc.style.display = "none";
 }
-//   if (x.style.display === "none" && y.style.display === "block" && z.style.display === "none") {
-//     x.style.display = "block";
-//     y.style.display = "none";
-//     z.style.display = "block";
-//   } else {
-//     x.style.display = "none";
-//     y.style.display = "block";
-//     z.style.display = "none";
-//   }
-// }
 
 function concluirComent(){
-  let x = document.getElementById('novoComent')
-  let y = document.getElementById('conteudo-disc')
-  let z = document.getElementById('caixa_novo_coment')
-  x.style.display = "none";
-  y.style.display = "block";
-  z.style.display = "block";
+  const novoComent = document.getElementById('novoComent')
+  const conteudoDisc = document.getElementById('conteudo-disc')
+  const caixaNovoComent = document.getElementById('caixa_novo_coment')
+  novoComent.style.display = "none";
+  conteudoDisc.style.display = "block";
+  caixaNovoComent.style.display = "block";
+
+
+  const txtEscreverNovoComent = document.getElementById('txtComp')
+  const tituloNovoComent = document.getElementById('assunto_novo_coment')
+  const textoNovoComent = document.getElementById('txt_novo_coment')
+  const assuntoNovoComent = document.getElementById('assunto')
+
+  tituloNovoComent.innerHTML = assuntoNovoComent.value
+  textoNovoComent.innerHTML = txtEscreverNovoComent.value
 
 }
 
 function mostrarComent(){
-  let x = window.document.getElementById('comentario')
-  if (x.style.display === "none") {
-    x.style.display = "block";
+  const comentario = window.document.getElementById('comentario')
+  if (comentario.style.display === "none") {
+    comentario.style.display = "block";
   } else {
-    x.style.display = "none";
+    comentario.style.display = "none";
   }
 }
 
 function mostrarComent2(){
-    let x = document.getElementById('comentario2')
-  if (x.style.display === "none") {
-    x.style.display = "block";
+    const comentario2 = document.getElementById('comentario2')
+  if (comentario2.style.display === "none") {
+    comentario2.style.display = "block";
   } else {
-    x.style.display = "none";
+    comentario2.style.display = "none";
   }
 }
